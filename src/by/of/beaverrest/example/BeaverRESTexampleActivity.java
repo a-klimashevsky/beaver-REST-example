41dddc8846dafcf6c726bc5e2ebc2cd18ee60e78
Alex Klimashevsky <gladiator1988@gmail.com>
2012-06-19 14:58:23 +0300

    init

---
+++ src/by/of/beaverrest/example/BeaverRESTexampleActivity.java
@@ -0,0 +1,13 @@
+package by.of.beaverrest.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeaverRESTexampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
